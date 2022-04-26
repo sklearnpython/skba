3551ed35bc9d818bfe70fae7afe81907fe97cede
sklearnpython <104429887+sklearnpython@users.noreply.github.com>
2022-04-26 12:24:48 +0200

    Add files via upload

---
+++ Xx.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xx;
+
+/**
+ *
+ * @author student
+ */
+public class Xx {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.println("Test");
+    
+    
+        System.out.println("Test2");
+        System.out.println("Podatci");
+        System.out.println();
+        
+        String x= "x";
+        
+      
+    }
+    
+
+}
+
+
